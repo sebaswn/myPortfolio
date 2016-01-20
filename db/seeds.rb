92431f59489b444a781e1fe437de10d70cdabc67
sebaswn <sebastiannee@yahoo.com>
2016-01-20 18:26:53 +0100

    starting the seeding process for the pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ Picture.destroy_all
 
 
 Picture.create(
-	name: "testing",
-	description: "Maybe this will work. Maybe not.",
+	name: "Cats",
+	description: "I took this picture almost on accident, and now it's become one of my favorites. This was in Russia, and the school I was volunteering at was almost infested with cats. These three were some of the newer kittens, and one of the ladies that worked there took a special liking to them. When she picked them up I could not help it and I had to take a picture. It's not often that one sees an armful of kittens.",
 	location: "Space",
 	coordinateY: 40.7127,
 	coordinateX: 74.0059,
