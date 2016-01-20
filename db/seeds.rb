ee0ddd6e34b0414f071bb8c799e659b8d45a4fa9
sebaswn <sebastiannee@yahoo.com>
2016-01-20 19:10:26 +0100

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,73 @@
 Picture.destroy_all
 
-
 Picture.create(
 	name: "Cats",
-	description: "I took this picture almost on accident, and now it's become one of my favorites. This was in Russia, and the school I was volunteering at was almost infested with cats. These three were some of the newer kittens, and one of the ladies that worked there took a special liking to them. When she picked them up I could not help it and I had to take a picture. It's not often that one sees an armful of kittens.",
-	location: "Space",
-	coordinateY: 40.7127,
-	coordinateX: 74.0059,
-	picURL: "http://i.imgur.com/ibTDCne.jpg")
+	location: "Kitezh, Russia",
+	picURL: "http://i.imgur.com/z06mv5a.jpg")
 
 Picture.create(
-	name: "testing2",
-	description: "Maybe this will work. Maybe not.",
-	location: "Space",
-	coordinateX: 40.7127,
-	coordinateY: 74.0059,
-	picURL: "http://i.imgur.com/ibTDCne.jpg")
+	name: "Grasshopping",
+	location: "Tucson, AZ, USA",
+	picURL: "http://i.imgur.com/Bxa4lnD.jpg")
+
+Picture.create(
+	name: "The Giant Grasshopper",
+	location: "Tucson, AZ, USA",
+	picURL: "http://i.imgur.com/1FEwedE.jpg")
+
+Picture.create(
+	name: "Hang on Tight",
+	location: "Tucson, AZ, USA",
+	picURL: "http://i.imgur.com/6CXr3nv.jpg")
+
+Picture.create(
+	name: "Asses",
+	location: "Tucson, AZ, USA",
+	picURL: "http://i.imgur.com/P4kMG9i.jpg")
+
+Picture.create(
+	name: "Loves me, loves me not",
+	location: "Kitezh, Russia",
+	picURL: "http://i.imgur.com/YH6tuKU.jpg")
+
+Picture.create(
+	name: "New York Square",
+	location: "New York, NY, USA",
+	picURL: "http://i.imgur.com/pWiQJOl.jpg")
+
+Picture.create(
+	name: "Up close and Personal",
+	location: "New York, NY, USA",
+	picURL: "http://i.imgur.com/1vUBHJf.jpg")
+
+Picture.create(
+	name: "New York Square",
+	location: "New York, NY, USA",
+	picURL: "http://i.imgur.com/pWiQJOl.jpg")
+
+Picture.create(
+	name: "Goldeneye",
+	location: "Tucson, AZ, USA",
+	picURL: "http://i.imgur.com/uXn7mzW.jpg")
+
+Picture.create(
+	name: "Too Close for Comfort",
+	location: "Tucson, AZ, USA",
+	picURL: "http://i.imgur.com/9BYteTL.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
